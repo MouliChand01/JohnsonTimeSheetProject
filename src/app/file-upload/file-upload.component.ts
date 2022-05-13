8a834dadfad20e18515a4fa76513531c2fe89f63
Ramadasu Mouli <Ramadasu Mouli@corp.ojas-it.com>
2022-05-13 15:23:29 +0530

    pastDate & filetype

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -9,23 +9,36 @@ import { OjasSerService } from '../ojas-ser.service';
 })
 export class FileUploadComponent implements OnInit {
   loginform: any;
-  constructor(private ser:OjasSerService) { }
+  errorMessgae:any;
 
+  constructor(private ser: OjasSerService) { }
   ngOnInit(): void {
     this.loginform = new FormGroup({
-      EmployeeName: new FormControl('',[Validators.required]),
-      employeeid: new FormControl('',[Validators.required]),
-      clientName: new FormControl("",[Validators.required]),
-      startDate: new FormControl("",[Validators.required]),
-      endDate: new FormControl("",[Validators.required]),
-      FileName: new FormControl("",[Validators.required])
+      empName: new FormControl('', [Validators.required]),
+      clientName: new FormControl("", [Validators.required]),
+      startDate: new FormControl("", [Validators.required]),
+      endDate: new FormControl("", [Validators.required]),
+      fileName: new FormControl("", [Validators.required])
     });
   }
-  
-  submited(){
+
+  submited() {
     console.log(this.loginform.value)
     alert(`fileUpload was done...!`);
     this.loginform.reset();
+    this.ser.fileUpload(this.loginform.value).subscribe((res)=>console.log(res))
   }
 
+  getToday(): string {
+    return new Date().toISOString().split('T')[0]
+  }
+  
+  onImageChangeFromFile($event: any) {
+      let file = $event.target.files[0];
+      if (file.type == "image/jpeg"  || file.type == 'image/png' || file.type == "application/pdf" ) {
+      }
+      else {
+        this.errorMessgae = "*Png,xlsx,jpeg Files are Required"
+      }
+  }
 }
--- src/app/ojas-ser.service.ts
+++ src/app/ojas-ser.service.ts
@@ -6,5 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class OjasSerService {
 
+  fileupload_url="http://localhost:9090/uploadFile";
+  timesheet_url="http://localhost:9090/addTimeSheet"
+
   constructor(private http:HttpClient) { }
+
+  fileUpload(data:any){
+    return this.http.post(this.fileupload_url,data)
+  }
 }
